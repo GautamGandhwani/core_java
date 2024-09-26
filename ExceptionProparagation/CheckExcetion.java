7e8bec1434b34cb7c8ee778af546bfdc62db472f
GautamGandhwani <gandhwanigautam@gmail.com>
2024-09-26 11:50:37 -0400

    done

---
+++ ExceptionProparagation/CheckExcetion.java
@@ -0,0 +1,24 @@
+package co.in.ExceptionProparagation;
+
+public class CheckExcetion {
+
+	public static void main(String[] args) {
+		try {
+			dad();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static void dad() throws Exception {
+		mom();
+	}
+
+	public static void mom() throws Exception {
+		son();
+	}
+
+	public static void son() throws Exception {
+		throw new Exception("hallo");
+	}
+}
